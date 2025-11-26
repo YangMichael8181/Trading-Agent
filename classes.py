Currently mostly working. Need to fix EMA calculation, incorrect at the moment.

classes.py
@@ -7,6 +7,9 @@ import queue
 from contextlib import redirect_stderr
 import io
 import parse
+import json
+
+import globals
 
 class NASDAQ:
 
@@ -18,34 +21,57 @@ class NASDAQ:
         period: string. Can be one of: 1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max. Default is 3mo
         interval: string. Can be one of: 1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo. NOTE: Intraday data cannot extend last 60 days
     """
+    # Collected stocks that match requirements
 
-    ticker_data = {}
+    # Requirements for stock
+    # Check folder config/default.json for example options
+    requirements = {}
+
+    # Parameters used to find and filter stock
+    # Interval: data interval (1m, 5m, 1w, 1d, etc.)
+    # Period: data period (1d, 5d, 3mo, 1y, etc.)
+    # Num_threads: 1 == # of producer_thread, (num_threads - 1) == # of consumer_threads
+    # Order: if order matters or not
+    # E.g. Some filter want EMA10 >= SMA20 >= SMA50 >= SMA100, while others want close_price > SMA20, SMA50, SMA100
+    # Use order to remember if we need to compare SMA prices with eachother or not
     interval = ""
     period = ""
     num_threads = 0
-    TIMEOUT_DURATION = 0
+    order = False
 
+    # Holds all filtered stock data
     ticker_dict_lock = threading.Lock()
+    ticker_data = {}
+
+    # NOTE: Queue class has built-in synchronization
     tickers = queue.Queue()
 
+    # Used to hold data retrieved from yfinance
     raw_data_lock = threading.Lock()
     raw_data = {}
 
-    tickers_gathered_lock = threading.Lock()
-
+    # Used to redirect stderr to a file
     output_lock = threading.Lock()
 
-    def __init__(self, period:str="3mo",interval:str="1d",num_threads:int = 5,timeout:int = 1):
+    # Cached MAs (for testing purposes)
+    cached_mas = {}
+    mas_lock = threading.Lock()
+
+#-------------------------------------------------STATE------------------------------------------------------------------------------
+
+    def __init__(self, json_file):
 
         # Initialize state
-        self.period = period
-        self.interval = interval
-        self.num_threads = num_threads
-        self.TIMEOUT_DURATION = timeout
+        self.period = json_file.get("period", "3mo")
+        self.interval = json_file.get("interval", "1d")
+        self.num_threads = json_file.get("num_threads", 5)
+        self.requirements = json_file.get("requirements", {"sma": [50]})
+        self.order = json_file.get("order", False)
+
 
         # Start producer/consumer threads
         threading.Thread(target=self._producer_func).start()
-        for _ in range(1, num_threads):
+        for _ in range(1, self.num_threads):
             threading.Thread(target=self._consumer_func).start()
 
         # Collect all tickers from datasets found online
@@ -54,23 +80,24 @@ class NASDAQ:
         # After everything processed, let producer and consumers know job is finished by putting None into the queue
         tickers_gathered = set()
         tickers_gathered.update(parse.gather_invalid_tickers())
-
         self._gather_nasdaq_tickers(tickers_gathered)
         self._gather_nyse_tickers(tickers_gathered)
         self.tickers.join()
         del tickers_gathered
         self.tickers.put(None)
-        
+
         # Cache results
-        with open("nasdaq_above_50sma.txt", "w") as file:
+        with open("nasdaq_above_50sma.txt", "w+") as file:
             for key in self.ticker_data.keys():
                 file.write(f"{key}\n")
+        
+        with open("verify_mas.json", "w+") as file:
+            json.dump(self.cached_mas, file, indent=4)
 
 
-#-----------------------------------------HELPERS-----------------------------------------------------------
+#---------------------------------------------GATHER-TICKERS-------------------------------------------------------------------------
 
-
-    def _gather_nasdaq_tickers(self, tickers_gathered:set):
+    def _gather_nasdaq_tickers(self, tickers_gathered:set) -> None:
         """
         Sends HTTP request to gather all tickers listed on the NASDAQ
         Parameters:
@@ -94,8 +121,13 @@ class NASDAQ:
         # skip last line: last line is newline, creates empty array and breaks
 
         # line[0]: ticker
-        # line[3]: test issue (test issue == fake stock, therefore DO NOT WANT)
-        # line[4]: financial status (financial_status != 'N' IS BAD DO NOT WANT)
+        # line[1]: company name (irrelevant)
+        # line[2]: market category (irrelevant)
+        # line[3]: test issue (test issue == fake stock)
+        # line[4]: financial status (financial_status == 'N' is only good option)
+        # line[5]: round lot size (irrelevant)
+        # line[6]: etf status (only want stocks)
+        # line[7]: nextshares (irrelevant)
         data = [list(line.split('|')) for line in data.split('\n')]
         tickers_list = [
             line[0]
@@ -103,6 +135,7 @@ class NASDAQ:
             in data[1:-2]
             if (line[3] != 'Y')
             and (line[4] == 'N')
+            and (line[6] == 'N')
             ]
             
         split_tickers_list = [tickers_list[i:i + 100] for i in range(0, len(tickers_list), 100)]
@@ -112,7 +145,7 @@ class NASDAQ:
         tickers_gathered.update(tickers_list)
 
 
-    def _gather_nyse_tickers(self, tickers_gathered:set):
+    def _gather_nyse_tickers(self, tickers_gathered:set) -> None:
         """
         Sends HTTP request to gather all tickers listed on the NYSE
         Requires another function because the data is listed in a different format
@@ -136,8 +169,12 @@ class NASDAQ:
         # skip last line: last line is newline, creates empty array and breaks
 
         # line[0]: ticker
-        # line[2]: exchange (Z and V are NOT NYSE, therefore IGNORE; P is NYSE ARCA, which does not hold many stocks)
-        # line[5]: test issue (test issue == fake stock, therefore DO NOT WANT)
+        # line[1]: company name (irrelevant)
+        # line[2]: exchange (Z and V are NOT NYSE, P is NYSE ARCA, which mainly holds ETFs)
+        # line[3]: cqs symbol (irrelevant)
+        # line[4]: etf status (do not want etfs)
+        # line[5]: round lot size (irrelevant)
+        # line[6]: test issue (test issue == fake stock, therefore DO NOT WANT)
         # line[7]: nasdaq_ticker (Ignore any tickers with non-letter characters (any warrant stocks, dividend stock, etc.))
         # line[7]: nasdaq_ticker (Already grabbed all nasdaq tickers, don't want duplicates)
         data = [list(line.split('|')) for line in data.split('\n')]
@@ -147,7 +184,8 @@ class NASDAQ:
             in data[1:-2]
             if (line[0].isalpha())
             and (line[2] != 'P' and line[2] != 'Z' and line[2] != 'V')
-            and (line[5] != 'Y')
+            and (line[4] == 'N')
+            and (line[6] != 'Y')
             and (line[7] not in tickers_gathered)
             ]
         
@@ -158,78 +196,7 @@ class NASDAQ:
         tickers_gathered.update(tickers_list)
 
 
-    def _gather_ticker_data(self, ticker:str):
-        """
-        Sends HTTP request to get price history for all tickers.
-        Parameters:
-            self
-            ticker: string. Should be a string with 100 tickers in the string, split by whitespace
-        """
-
-        # Send API request to API to gather data
-        # Capture stderr output, save to file (since delisted tickers do not raise an exception)
-        print(f"grabbing price history for {ticker}. . .")
-        stderr_output = io.StringIO()
-        try:
-            with self.output_lock:
-                    with redirect_stderr(stderr_output):
-                        # data = yf.Tickers(ticker).history(period="3mo",interval="1d")
-                        data = yf.Tickers("CTEST").history(period="3mo",interval="1d")
-        except Exception as e:
-            print(f"\nEXCEPTION OCCURED: {e}")
-
-
-        with open("test.txt", "a") as file:
-            file.write(stderr_output.getvalue())
-        return
-        split_tickers = ticker.split(" ")
-        for ticker in split_tickers:
-            key = ('Close', ticker)
-            if key not in data.columns:
-                continue
-            frame = data[key]
-
-
-            # Check to see if there are 50 days of information
-            # Especially for new stocks, may not have 50 days worth of information
-            if len(frame) < 50:
-                return None
-            
-            with self.raw_data_lock:
-                self.raw_data[ticker] = frame
-
-
-    def _process_ticker_data(self, ticker:str, price_frame):
-        """
-        Processes data from tickers to calculate 50 day simple-moving-average
-        Parameters:
-            self
-            ticker: string. Singular ticker
-            price_frame: Pandas Dataframe. Returned by the yfinance API, contains price data from the past 3 months
-        """
-        # TODO: implement verbose mode
-        # print("Processing raw_data . . .")
-        # Calculate 50 day simple moving average
-        # Grab latest close price, round it to 2 decimal places
-        # Sometimes yfinance API returns close price as an array, so try . . . except
-        sma_50 = round(price_frame.tail(50).mean(), 2)
-        try:
-            latest_close = round(price_frame.tail(1).item(), 2)
-
-        except Exception as e:
-            latest_close = round(price_frame.tail(1).mean(), 2)
-        
-        # If faulty data, ignore
-        # Only save tickers that are above 50 day simple moving average
-        # Save price frame to calculate other filters required
-        if np.isnan(sma_50) or np.isnan(latest_close):
-            return
-        if latest_close > sma_50:
-            with self.ticker_dict_lock:
-                self.ticker_data[ticker] = price_frame
-
-
-#--------------------PRODUCER-CONSUMER-FUNCTIONS-------------------------------------------------------------------
+#-------------------------------------------PRODUCER-FUNCTIONS-----------------------------------------------------------------------
 
 
     def _producer_func(self):
@@ -248,23 +215,212 @@ class NASDAQ:
             self._gather_ticker_data(ticker)
             self.tickers.task_done()
             time.sleep(4)
-    
+
+
+    def _gather_ticker_data(self, ticker:str) -> None:
+        """
+        Sends HTTP request to get price history for all tickers.
+        Parameters:
+            self
+            ticker: string. Should be a string with 100 tickers in the string, split by whitespace
+        """
+
+        # Send API request to API to gather data
+        # Rate-limit raises exception, thus need except block
+        # Unable to find tickers DO NOT raise an exception, prints to stderr
+        # Save stderr to file, check file for invalid tickers, add to ignore list
+        print(f"grabbing price history for {ticker}. . .")
+        stderr_output = io.StringIO()
+        try:
+            with self.output_lock:
+                    with redirect_stderr(stderr_output):
+                        data = yf.download(tickers=ticker, period=self.period,interval=self.interval, auto_adjust=True)
+        except Exception as e:
+            print(f"\nEXCEPTION OCCURED: {e}")
+
+        with open(globals.ERROR_MESSAGE_FILE, "a+") as file:
+            file.write(stderr_output.getvalue())
+
+        # Search through price frames
+        split_tickers = ticker.split(" ")
+        for ticker in split_tickers:
+            for key in data.columns.tolist():
+                price, ticker = key
+                frame = data[key]
+                with self.raw_data_lock:
+                    if price not in self.raw_data.keys():
+                        self.raw_data[price] = {}
+                    if ticker not in self.raw_data[price].keys():
+                        self.raw_data[price][ticker] = {}
+
+                    self.raw_data[price][ticker] = frame
+
+
+#-------------------------------------------CONSUMER-FUNCTIONS-----------------------------------------------------------------------
+
+
     def _consumer_func(self):
         # Infinite loop so consumer runs until job is finished
         # Check dict for work; if "<Producer_Complete>" token exists in dict, means producer put it there, job is done
         # Otherwise, process tickers, put into ticker_data
         # Keep repeating process until queue is finished, gives more time between API calls
         while True:
-            ticker_to_process = None
-            frame_to_process = None
+            ticker = None
+            frame = None
             with self.raw_data_lock:
-                if len(self.raw_data) > 0:
-                    if self.raw_data.get("<Producer_Complete>", None) is not None:
+                if self.raw_data.get("<Producer_Complete>", None) is not None:
                         break
-                    ticker_to_process, frame_to_process = self.raw_data.popitem()
+                close_dict = self.raw_data.get("Close", None)
+                if close_dict is not None and len(close_dict) > 0:
+                    ticker, frame = close_dict.popitem()
             
-            if ticker_to_process is not None and frame_to_process is not None:
-                self._process_ticker_data(ticker_to_process, frame_to_process)
+            if ticker is not None and frame is not None:
+                self._process_ticker_data(ticker, frame)
             else:
                 time.sleep(0.1)
 
+
+    def _process_ticker_data(self, ticker:str, frame) -> None:
+        """
+        Processes data from tickers to calculate 50 day simple-moving-average
+        Parameters:
+            self
+            ticker: string. Singular ticker
+            frame: Pandas Dataframe. Returned by the yfinance API, contains price data from the past 3 months
+        """
+        
+
+        if not self._validate_mas(ticker=ticker, frame=frame, ma_list=self.requirements["mas"]):
+            return
+
+        # If valid, add ticker and corresponding frame to memory
+        with self.ticker_dict_lock:
+            self.ticker_data[ticker] = frame
+
+
+#----------------------------------------------TECHNICALS----------------------------------------------------------------------------
+
+
+    def _validate_mas(self, ticker:str, frame, ma_list:list) -> bool:
+        """
+        Calculates the SMA using the given frame for the given ticker. Assumes 1d timeframe
+        If enough data exists, returns SMA rounded to 2 decimal points for given range.
+        Otherwise, if lacking data, returns -1.0
+        E.g. if calculating SMA 200 for stock that has 100 days of data, not possible. Return -1.0
+
+        Parameters:
+            self
+            ticker: string. Singular ticker
+            frame: Pandas Dataframe. Returned by the yfinance API, contains price data
+        
+        """
+        # Grab latest close price from frame
+        # Ensure price is valid
+
+        write_dict = {}
+        try:
+            latest_close = round(frame.tail(1).item(), 2)
+        except Exception as e:
+            latest_close = round(frame.tail(1).mean(), 2)
+        if np.isnan(latest_close):
+            return False
+        
+        write_dict["latest_close"] = latest_close
+        # Compare MA values
+        prev_value = latest_close
+        for ma in ma_list:
+            ma = ma.upper()
+            time_range = int(ma[3:])
+            if "SMA" in ma:
+                calc_val = self._calculate_sma(frame=frame, time_range=time_range)
+            elif "EMA" in ma:
+                calc_val = self._calculate_ema(frame=frame, time_range=time_range, smoothing=2)
+            
+            if np.isnan(calc_val):
+                return False
+            elif prev_value < calc_val:
+                return False
+            
+            write_dict[ma] = calc_val
+            if self.order:
+                prev_value = calc_val
+
+        with self.mas_lock:
+            self.cached_mas[ticker] = write_dict
+            with open("verify_mas.json", "w+") as file:
+                json.dump(self.cached_mas, file, indent=4)
+        return True
+
+
+    def _calculate_ema(self, frame, time_range:int = 50, smoothing:int = 2) -> float:
+        """
+        Calculates the EMA using the given frame for the given ticker. Assumes 1d timeframe
+        If enough data exists, returns EMA rounded to 2 decimal points for given range.
+        Otherwise, if lacking data, returns -1.0
+        E.g. if calculating EMA 200 for stock that has 100 days of data, not possible. Return -1.0
+
+        Parameters:
+            self
+            ticker: string. Singular ticker
+            frame: Pandas Dataframe. Returned by the yfinance API, contains price data
+            time_range: int. the number of days to calculate the MA. e.g. EMA10, time_range == 10. Default is 50
+            smoothing: int. Smoothing factor for algorithm. Default is 2
+        """
+
+        # Check if stock has enough data to calculate for provided range
+        if len(frame) + 1 < time_range:
+                return -1.0
+        
+        try:
+            latest_close = round(frame.tail(1).item(), 2)
+        except Exception as e:
+            latest_close = round(frame.tail(1).mean(), 2)
+            
+        frame = frame[ :time_range + 1]
+        frame = frame[ :-1]
+        
+        factor = smoothing / (1 + time_range)
+        sma = self._calculate_sma(frame, time_range)
+
+        today_ema = factor * latest_close
+        yesterday_ema = sma * (1 - factor)
+        return round((today_ema + yesterday_ema), 2)
+
+
+    def _calculate_sma(self, frame, time_range:int = 50) -> float:
+        """
+        Calculates the EMA using the given frame for the given ticker. Assumes 1d timeframe
+        If enough data exists, returns EMA rounded to 2 decimal points for given range.
+        Otherwise, if lacking data, returns -1.0
+        E.g. if calculating EMA 200 for stock that has 100 days of data, not possible. Return -1.0
+
+        Parameters:
+            self
+            ticker: string. Singular ticker
+            frame: Pandas Dataframe. Returned by the yfinance API, contains price data
+            time_range: int. the number of days to calculate the MA. e.g. EMA10, time_range == 10. Default is 50
+            smoothing: int. Smoothing factor for algorithm. Default is 2
+        """
+        # Check if stock has enough data to calculate for provided range
+        if len(frame) < time_range:
+                return -1.0
+        
+        return round(frame.tail(time_range).mean(), 2)
+
+
+    def calculate_volume(self, ticker, frame, range) -> dict:
+        """
+        Calculates:
+        latest volume: volume of latest day
+        average volume: average volume of last range days
+        relative volume: ratio of latest volume : average volume
+        Returns in a dict {latest_volume: <value>, . . .}
+
+        Parameters:
+            self
+            ticker: string. Singular ticker
+            frame: Pandas Dataframe. Returned by the yfinance API, contains historical data
+
+        
+        """
+        return